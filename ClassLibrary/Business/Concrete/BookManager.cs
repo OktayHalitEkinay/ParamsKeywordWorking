05be31bc2ec71efba607886f649dbcff5b52dbf5
OktayHalitEkinay <67113059+OktayHalitEkinay@users.noreply.github.com>
2021-10-01 14:09:48 +0300

    Add project files.

---
+++ ClassLibrary/Business/Concrete/BookManager.cs
@@ -0,0 +1,32 @@
+﻿using ClassLibrary.Core.Utilities.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Business.Concrete
+{
+    public class BookManager
+    {
+        public string Add()
+        {
+            if (BusinessRules.Run(CheckIfBookExists(),CheckIfBookNameLower3()))
+            {
+                return "Fail.";
+            }
+            return "The book added to db.";
+        }
+
+        //Business Rules
+        private bool CheckIfBookExists()
+        {
+            return false;//The book does not exist in db.
+        }
+        private bool CheckIfBookNameLower3()
+        {
+            return false;//The book name longer than 3.
+        }
+
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,15 @@
+﻿using ClassLibrary.Business.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BookManager bookManager = new BookManager();
+            var result=bookManager.Add();
+            Console.WriteLine(result);
+        }
+    }
+}
